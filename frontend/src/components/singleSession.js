040b7038aa917f512c6e2dd0b7d08bf439f24c69
Gasiek <kacper.b.gasior@gmail.com>
2019-01-19 00:16:34 +0100

    uploading

--- frontend/src/components/singleSession.js
+++ frontend/src/components/singleSession.js
@@ -5,29 +5,14 @@ import Hour from './Hour';
 
 class singleSession extends React.Component {
     state = {
-        hours: []
+        hours: [],
+        clicked: false
     }
-
+    
     buttonContainer = () => {
          return this.props.styling
     }
-
-    getHours = () => {
-        var toArray = Object.values(this.props.sessions)
-        var days = this.props.sessions.filter((toArray)=> toArray.date === this.props.uniqueSession);
-        console.log(days[0].time)
-        return days.map((hour)=>(<Hour key={hour.time} hour={hour.time}/>))
-    }
     
-    render() {
-        return (
-            <div>
-                <Button variant="outlined" color="default" style={this.styleButton()} onClick={this.getHours}> {this.props.uniqueSession} </Button>
-                <div style={this.buttonContainer()}> {this.getHours()} </div>
-            </div>
-        )
-    }
-
     styleButton = () => {
         return {
             border: '2px black solid',
@@ -36,6 +21,27 @@ class singleSession extends React.Component {
             margin: '20px'
         }
     }
+
+    handleClick = () => {
+        return this.setState({clicked:true})
+    }
+    
+    getHours = () => {
+        if(this.state.clicked){
+        var toArray = Object.values(this.props.sessions)
+        var days = this.props.sessions.filter((toArray)=> toArray.date === this.props.uniqueSession);
+        return days.map((hour)=>(<Hour key={hour.time} hour={hour.time}/>))}
+    }
+
+    render() {
+        return (
+            <div>
+                <Button variant="outlined" color="default" style={this.styleButton()} onClick={this.handleClick}> {this.props.uniqueSession} </Button>
+                <div style={this.buttonContainer()}> {this.getHours()} </div>
+            </div>
+        )
+    }
+    
 
     
 
